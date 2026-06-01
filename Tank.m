7f071ecc05c9a360260e2b57f9b3af3a7811320e
naderalftayeh <72920333+naderalftayeh@users.noreply.github.com>
2026-06-01 14:21:24 +0200

    Add files via upload

---
+++ Tank.m
@@ -0,0 +1,17 @@
+
+%Abflusssfläche: [m^2]
+ a=1.963e-05; 
+
+%Konstant der Pumpe: [m^3/vs]
+K=2.500e-06;
+
+% Fläche Tank1: [m^2]
+At1=2.3*10^-3;
+iAt1=1/At1;
+
+% Fläche Tank2: [m^2]
+At2=3.9*10^-3;
+iAt2= 1/At2;
+
+% G=2g : [m/s^2]
+G=2*9.81;  
